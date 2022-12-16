4ea0116029e2cb78492b0c061e20a1c8ca725a3e
Shubham Pal <115977664+beincharacter@users.noreply.github.com>
2022-12-16 13:04:02 +0530

    Update postRoutes.js
    added cors

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -16,6 +16,13 @@ mongoose.connect(uri, (err)=>{
   }
 });
 
+
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
+
 const router = express.Router();
 
 router.use(bodyparser.json());
